cc20d0698db1909c202cddc8c191cbd59b439bba
Matheus Albuquerque <mvca@cin.ufpe.br>
2023-11-06 23:04:38 -0300

    fix: removed unused function

--- Middleware/App/server/middleware/server.go
+++ Middleware/App/server/middleware/server.go
@@ -2,24 +2,7 @@ package main
 
 import (
 	"Middleware/Distribution/invoker"
-	"fmt"
-	"io/ioutil"
-	"os"
 )
-
-func lerLivro(nomeLivro string) []byte {
-	file, err := os.Open("./books/" + nomeLivro + ".txt")
-	if err != nil {
-		fmt.Println(err)
-	}
-	defer file.Close()
-
-	content, err := ioutil.ReadAll(file)
-	if err != nil {
-		fmt.Println(err)
-	}
-	return content
-}
 
 func main() {
 	invoker := invoker.Invoker{}
